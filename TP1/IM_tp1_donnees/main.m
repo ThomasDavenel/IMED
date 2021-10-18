281119b841ee0ed6358aa7e474d7562579a40652
18001462 <thomas.davenel@outlook.com>
2021-10-18 15:26:12 +0200

    on range tout

--- TP1/IM_tp1_donnees/main.m
+++ TP1/IM_tp1_donnees/main.m
@@ -1,7 +1,7 @@
 close all;
 clear;clc
 
-load('IRM_cerveau_avecbiais.mat')
+load('IRM_cerveau.mat')
 test = squeeze(M0(:,:,80));
 figure(1);
 subplot(1,2,1)
@@ -18,4 +18,4 @@ imagesc(Segmentation);
 
 volumeSeg = k_moyennes_Volume(M0,1,3);
 volumeSeg = smooth3(volumeSeg);
-MarchingCubes(volumeSeg,1);
+MarchingCubes(volumeSeg,1.5);
